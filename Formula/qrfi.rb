85483e1abcf73653af13999999feaafb6387d605
Hidetomo Hosono <h12o+homebrew@blessedgeeks.com>
2026-03-20 21:38:42 +0900

    ci: fix workflows and formula style

--- Formula/qrfi.rb
+++ Formula/qrfi.rb
@@ -1,10 +1,9 @@
 class Qrfi < Formula
-  desc "A CLI Wi-Fi QR Code Generator written in Rust"
+  desc "CLI Wi-Fi QR Code Generator written in Rust"
   homepage "https://github.com/h12o/qrfi"
-  version "0.1.0"
-  license "MIT"
   url "https://github.com/h12o/qrfi/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "622ab55533c2ac1d5c3d1ab2f9fd718cace9f37685dbbf245c36f8cf0ab915b9"
+  license "MIT"
   head "https://github.com/h12o/qrfi.git", branch: "main"
   depends_on "rust" => :build
   def install
