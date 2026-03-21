1aeb193d6b837cc499289e0b22bb90d21559b1c2
Hidetomo Hosono <h12o+homebrew@blessedgeeks.com>
2026-03-21 21:49:23 +0900

    feat: update homepage to custom domain
    Closes #2.
    
    Signed-off-by: Hidetomo Hosono <h12o+homebrew@blessedgeeks.com>

--- Formula/qrfi.rb
+++ Formula/qrfi.rb
@@ -1,6 +1,6 @@
 class Qrfi < Formula
   desc "CLI Wi-Fi QR Code Generator written in Rust"
-  homepage "https://github.com/h12o/qrfi"
+  homepage "https://h12o.blessedgeeks.com/qrfi"
   url "https://github.com/h12o/qrfi/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "622ab55533c2ac1d5c3d1ab2f9fd718cace9f37685dbbf245c36f8cf0ab915b9"
   license "MIT"
